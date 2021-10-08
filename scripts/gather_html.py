1d3c63bc7413f3328c6ccc21b9224f10879040da
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2021-10-08 16:50:41 -0500

    Collected location pages (contains details about most---if not all---trainers)

---
+++ scripts/gather_html.py
@@ -0,0 +1,22 @@
+import requests
+import re
+file = open('../data/location_urls','r')
+Lines = file.readlines()
+
+for url in Lines:
+    # Create file name
+    url = url.strip()
+    try:
+        out_file_name = re.search('=(.+?)&action=edit', url).group(1)
+    except:
+        out_file_name = 'failed'
+    out_file_name = '../html_sources/' + out_file_name + '.html'
+
+    # open output file
+    out_file = open(out_file_name, 'w')
+
+    # Write HTML Source
+    contents = requests.get(url)
+    out_file.write(contents.text)
+
+    print(out_file_name)
