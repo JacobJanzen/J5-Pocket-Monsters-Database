ff2ae4b234eb21bcdba727772ae801d95ab3d50c
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2021-10-17 15:07:54 -0500

    Created parties table

--- scripts/trainer_to_id.py
+++ scripts/trainer_to_id.py
@@ -1,5 +1,5 @@
 trainer_file = open('../data/trainers.csv')
-file = open('../data/teams.csv')
+file = open('../data/parties')
 Lines = file.readlines()
 Trainers = trainer_file.readlines()
 
@@ -7,15 +7,17 @@ trainers = dict()
 
 for line in Trainers:
     words = line.strip().split(',')
-    if len(words) > 2:
-        name = words[1] + ',' + words[2]
-        trainers[name] = words[0]
+    name = words[1] + ',' + words[2]
+    trainers[name] = words[0]
 
 for line in Lines:
-    words = line.strip().split(',')
+    words = line.strip().split('|')
     name = words[0] + ',' + words[1]
+    rest = ''
+    for i in range(2,len(words)):
+        rest += words[i] + '|'
     trainer_num = trainers.get(name)
     if trainer_num == None:
-        print('{},{}'.format(name,words[2]))
+        print('{}|{}'.format(name,rest))
     else:
-        print('{},{}'.format(trainer_num,words[2]))
+        print('{}|{}'.format(trainer_num,rest))
