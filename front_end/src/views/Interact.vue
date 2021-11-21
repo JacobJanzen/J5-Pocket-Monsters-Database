006ababa8df9f7b3ed48290564eae38067c6c007
jaredwebber <jaredwebberdev@gmail.com>
2021-11-21 00:10:49 -0600

    messed up the interact page

--- front_end/src/views/Interact.vue
+++ front_end/src/views/Interact.vue
@@ -3,45 +3,57 @@
         <h1>Interact with the Database!</h1>
 
         <h3>Select which info you're looking for in the drop-down below</h3>
-          <v-card>
-            <v-container fluid>
-              <v-row
-                align="center"
-              >
-                <v-col cols="12">
-                  <v-autocomplete
-                    v-model="value"
-                    :items="items"
-                    dense
-                    filled
-                    label="What do you want to know?"
-                  ></v-autocomplete>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card> 
+    <v-container fluid>
+      <v-row align="center">
+        <v-col cols="6">
+          <v-subheader>
+            Custom items
+          </v-subheader>
+        </v-col>
+
+        <v-col cols="6">
+          <v-select
+            v-model="select"
+            :hint="`${select.state}, ${select.abbr}`"
+            :items="items"
+            item-text="state"
+            item-value="abbr"
+            label="Select"
+            persistent-hint
+            return-object
+            single-line
+          ></v-select>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <!-- this shit dont work https://vuetifyjs.com/en/components/selects/ -->
 
             <br>
             <h3>BASED ON ABOVE QUERY SELECTION, HAVE MORE DROP-DOWNS TO SELECT PARAMETERS</h3>
             <h3>Probably use a different auto-complete type which shows all options easily</h3>
 
-            <!-- https://blog.devgenius.io/vuetify-autocomplete-de19719dff0d -->
+            <!-- Maybe use combobox instead - only allow single selection at a time ?
+                  with 'change' event to trigger parameter popup options?-->
 
 
     </div>
 </template>
 
-<script id="querySelector">
-   export default {
-    data: () => ({
-      items: [
-        'why is jared the dumbest person on earth', 
-        'what does simp mean', 
-        'i dont know anything about pokemon', 
-        'hell ya more queries'],
-      values: ['foo', 'bar'],
-      value: null,
-    }),
+<script>
+  export default {
+    data () {
+      return {
+        select: { state: 'Florida', abbr: 'FL' },
+        items: [
+          { state: 'Florida', abbr: 'FL' },
+          { state: 'Georgia', abbr: 'GA' },
+          { state: 'Nebraska', abbr: 'NE' },
+          { state: 'California', abbr: 'CA' },
+          { state: 'New York', abbr: 'NY' },
+        ],
+      }
+    },
   }
 </script>
 
