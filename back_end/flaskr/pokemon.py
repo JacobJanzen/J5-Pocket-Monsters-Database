8e3072415af0d5683234d2d5a2d8e1578586ab03
dlarocque <dlarocque.contact@gmail.com>
2021-11-22 16:11:53 -0600

    Added requests for the dropdowns

--- back_end/flaskr/pokemon.py
+++ back_end/flaskr/pokemon.py
@@ -7,19 +7,37 @@ from flaskr.db import get_db
 bp = Blueprint('pokemon', __name__, url_prefix='/pokemon')
 
 
-@bp.route('/test')
-def test():
+@bp.route('/listPokemonNames')
+def list_pokemon_names():
+    """Lists all of the pokemon names"""
     con = get_db()
     cur = con.cursor()
-    cur.execute('SELECT * FROM Pokemon')  # sample
+    cur.execute('SELECT PokemonName FROM Pokemon')
 
     d = {}
-
     for row in cur.fetchall():
-        for key in row.keys():
-            if key not in d:
-                d[key] = [str(row[key])]
+        for col in row.keys():
+            if col not in d:
+                d[col] = [str(row[col])]
             else:
-                d[key].append(str(row[key]))
+                d[col].append(str(row[col]))
+
+    return json.dumps(d)
+
+
+@bp.route('/listMoveNames')
+def list_move_names():
+    """Lists all of the pokemon names"""
+    con = get_db()
+    cur = con.cursor()
+    cur.execute('select MoveName from Move')
+
+    d = {}
+    for row in cur.fetchall():
+        for col in row.keys():
+            if col not in d:
+                d[col] = [str(row[col])]
+            else:
+                d[col].append(str(row[col]))
 
     return json.dumps(d)
