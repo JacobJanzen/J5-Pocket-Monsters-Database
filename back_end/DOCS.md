c4e391480ea1e40a3056f13bbad078e9089e9502
dlarocque <dlarocque.contact@gmail.com>
2021-11-23 17:22:15 -0600

    Added Queries

--- back_end/DOCS.md
+++ back_end/DOCS.md
@@ -16,6 +16,12 @@ This contains the names of all the implemented API methods in the Flask app.
 | pokemon_stats | GET | pokemon_name | Lists the stats of a pokemon |
 | pokemon_evolutions | GET | pokemon_name | Lists the evolutions of a pokemon |
 | pokemon_with_move | GET | move_name | Lists the pokemon with a given move |
+| pokemon_from_types_with_highest_stat | GET | stat_name | Lists the pokemon from each type with the highest value in a given stat |
+| pokemon_from_types_with_lowest_stat | GET | stat_name | Lists the pokemon from each type with the lowest value in a given stat |
+| pokemon_with_stat_greater_than | GET | stat_name, min_value | Lists the pokemon having a minimum value of a given stat |
+| pokemon_with_stat_less_than | GET | stat_name, max_value | Lists the pokemon having a maximum value of a given stat |
+| pokemon_can_be_caught_at_location | GET | location_name | Lists the pokemon that can be caught at a given location |
+| pokemon_can_be_caught_at_location_from_encounter | GET | location_name, encounter_name | Lists the pokemon that can be caught at a given location from a given encounter |
 
 
 ## Move Selection
--- back_end/flaskr/pokemon.py
+++ back_end/flaskr/pokemon.py
@@ -95,21 +95,131 @@ def pokemon_evolutions(pokemon_name: str):
 
 @bp.route('/pokemon_with_move/<move_name>')
 def pokemon_with_move(move_name: str):
-    """Lists the evolutions of a pokemon"""
+    """Lists the pokemon with a given move"""
     con = get_db()
     cur = con.cursor()
     cur.execute(f'''
-                select PokemonName, Learns.method from Pokemon natural join Learns natural join Move
+                select PokemonName from Pokemon natural join Learns natural join Move
                 where Move.MoveName = "{move_name}"
                 union
-                select PokemonName from Pokemon natural join LearnsByBreeding natural join Move
-                where Move.MoveName = "{move_name}"
+                select PokemonName from Pokemon natural join LearnsByBreeding natural join Move 
+                 where Move.MoveName = "{move_name}"
                 ''')
-    # left and right side of unions do not have the same number of columns
 
     d = {}
     for row in cur.fetchall():
         for col in row.keys():
-            d[col] = row[col]
+            if col not in d:
+                d[col] = [row[col]]
+            else:
+                d[col].append(row[col])
+
+    return json.dumps(d)
+
+
+@bp.route('/pokemon_from_types_with_highest_stat/<stat_name>')
+def pokemon_from_types_with_highest_stat(stat_name: str):
+    """Lists the pokemon from each type with the highest value in a given stat"""
+    con = get_db()
+    cur = con.cursor()
+    cur.execute(f'''
+                select TypeName, PokemonName, "{stat_name}" from Pokemon natural join HasTypes
+                group by TypeName
+                having max("{stat_name}")
+                order by "{stat_name}" desc
+                ''')
+
+    d = {}
+    for row in cur.fetchall():
+        d[row[0]] = row[1]
+
+    return json.dumps(d)
+
+
+@bp.route('/pokemon_from_types_with_lowest_stat/<stat_name>')
+def pokemon_from_types_with_lowest_stat(stat_name: str):
+    """Lists the pokemon from each type with the lowest value in a given stat"""
+    con = get_db()
+    cur = con.cursor()
+    cur.execute(f'''
+                select TypeName, PokemonName, "{stat_name}" from Pokemon natural join HasTypes
+                group by TypeName
+                having min("{stat_name}")
+                order by "{stat_name}" desc
+                ''')
+
+    d = {}
+    for row in cur.fetchall():
+        d[row[0]] = row[1]
+
+    return json.dumps(d)
+
+
+@bp.route('/pokemon_with_stat_greater_than/<stat_name>&<min_value>')
+def pokemon_with_stat_greater_than(stat_name: str, min_value: int):
+    """Lists the pokemon having a minimum value of a given stat"""
+    con = get_db()
+    cur = con.cursor()
+    cur.execute(f'''
+                select PokemonName, "{stat_name}" from Pokemon
+                where "{stat_name}" >= "{min_value}"
+                order by "{stat_name}"
+                ''')
+
+    d = {}
+    for row in cur.fetchall():
+        d[row[0]] = row[1]
+
+    return json.dumps(d)
+
+
+@bp.route('/pokemon_with_stat_less_than/<stat_name>&<max_value>')
+def pokemon_with_stat_less_than(stat_name: str, max_value: int):
+    """Lists the pokemon having a maximum value of a given stat"""
+    con = get_db()
+    cur = con.cursor()
+    cur.execute(f'''
+                select PokemonName, "{stat_name}" from Pokemon
+                where "{stat_name}" <= "{max_value}"
+                order by "{stat_name}"
+                ''')
+
+    d = {}
+    for row in cur.fetchall():
+        d[row[0]] = row[1]
+
+    return json.dumps(d)
+
+
+@bp.route('/pokemon_can_be_caught_at_location/<location_name>')
+def pokemon_can_be_caught_at_location(location_name: str):
+    """Lists the pokemon that can be caught at a given location"""
+    con = get_db()
+    cur = con.cursor()
+    cur.execute(f'''
+                select Dex, PokemonName, Encounter from Pokemon natural join FoundAt
+                where FoundAt.LocationName = "{location_name}"
+                ''')
+
+    d = {}
+    for row in cur.fetchall():
+        d[row[1]] = {row.keys()[0]: row[0], row.keys()[2]: row[2]}
+
+    return json.dumps(d)
+
+
+@bp.route('/pokemon_can_be_caught_at_location_from_encounter/<location_name>&<encounter_name>')
+def pokemon_can_be_caught_at_location_from_encounter(location_name: str, encounter_name: str):
+    """Lists the pokemon that can be caught at a given location from a given encounter"""
+    con = get_db()
+    cur = con.cursor()
+    cur.execute(f'''
+                select Dex, PokemonName, Encounter from Pokemon natural join FoundAt
+                where FoundAt.LocationName = "{location_name}" and Encounter = "{encounter_name}";   
+                ''')
+
+    d = {}
+    for row in cur.fetchall():
+        d[row[1]] = {row.keys()[0]: row[0], row.keys()[2]: row[2]}
 
     return json.dumps(d)
