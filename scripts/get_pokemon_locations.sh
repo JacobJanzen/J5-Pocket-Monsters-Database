f24456a4c1841be9854eb978634a188fff5bd02e
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2021-11-23 13:26:45 -0600

    removed unnecessary files

--- scripts/get_pokemon_locations.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for file in ../markdown_source/*; do
-    base=$(basename "${file}")
-    echo ${base}
-    python extract_section.py Pokémon ${file} >> ../data/pokemon_locations
-done
--- scripts/make_2_columns.py
+++
@@ -1,9 +0,0 @@
-# Used to convert consecutive lines into 2 columns of CSV
-file = open('data/pokedex.ts','r')
-while True:
-    line1 = file.readline()
-    line2 = file.readline()
-    if not line2: break
-
-    print('{},'.format(line1.strip()),end='')
-    print('{}'.format(line2.strip()))
