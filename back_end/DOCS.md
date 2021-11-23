c79b09bd040e582f0d83e9f988ce6adeb27af686
dlarocque <dlarocque.contact@gmail.com>
2021-11-22 18:05:10 -0600

    Added queries

--- back_end/DOCS.md
+++ back_end/DOCS.md
@@ -4,11 +4,19 @@ This contains the names of all the implemented API methods in the Flask app.
 ## Search and Drop-downs
 
 |Method Name |Type |Description|
-|------------|-----|-----------|
-| list_pokemon_names | GET | Lists all of the pokemon names |
-| list_move_names | GET | Lists all of the move names |
+|---|---|---|
+| pokemon_names | GET | Lists all of the pokemon names |
+| move_names | GET | Lists all of the move names |
 
 ## Pokemon Selection
+
+|Method Name |HTTP Method | Parameters | Description|
+|---|---|---|---|
+| dex_pokemon_names | GET |  | Lists all of the pokemon names and their position in the Pokedex|
+| pokemon_stats | GET | pokemon_name | Lists the stats of a pokemon |
+| pokemon_evolutions | GET | pokemon_name | Lists the evolutions of a pokemon |
+| pokemon_with_move | GET | move_name | Lists the pokemon with a given move |
+
 
 ## Move Selection
 
--- back_end/flaskr/pokemon.py
+++ back_end/flaskr/pokemon.py
@@ -7,12 +7,12 @@ from flaskr.db import get_db
 bp = Blueprint('pokemon', __name__, url_prefix='/pokemon')
 
 
-@bp.route('/listPokemonNames')
-def list_pokemon_names():
+@bp.route('/pokemon_names')
+def pokemon_names():
     """Lists all of the pokemon names"""
     con = get_db()
     cur = con.cursor()
-    cur.execute('SELECT PokemonName FROM Pokemon')
+    cur.execute('select PokemonName from Pokemon')
 
     d = {}
     for row in cur.fetchall():
@@ -25,8 +25,8 @@ def list_pokemon_names():
     return json.dumps(d)
 
 
-@bp.route('/listMoveNames')
-def list_move_names():
+@bp.route('/move_names')
+def move_names():
     """Lists all of the move names"""
     con = get_db()
     cur = con.cursor()
@@ -39,5 +39,77 @@ def list_move_names():
                 d[col] = [str(row[col])]
             else:
                 d[col].append(str(row[col]))
+
+    return json.dumps(d)
+
+
+@bp.route('/dex_pokemon_names')
+def dex_pokemon_names():
+    """Lists all of the pokemon names and their position in the Pokedex"""
+    con = get_db()
+    cur = con.cursor()
+    cur.execute('select Dex, PokemonName from Pokemon')
+
+    d = {}
+    for row in cur.fetchall():
+        d[row[0]] = row[1]
+
+    return json.dumps(d)
+
+
+@bp.route('/pokemon_stats/<pokemon_name>')
+def pokemon_stats(pokemon_name: str):
+    """Lists the stats of a pokemon"""
+    con = get_db()
+    cur = con.cursor()
+    cur.execute(f'''
+                select HP, Atk, Def, SpA, SpD, Spe from Pokemon
+                where PokemonName = "{pokemon_name}"
+                ''')
+
+    d = {}
+    for row in cur.fetchall():
+        for col in row.keys():
+            d[col] = row[col]
+
+    return json.dumps(d)
+
+
+@bp.route('/pokemon_evolutions/<pokemon_name>')
+def pokemon_evolutions(pokemon_name: str):
+    """Lists the evolutions of a pokemon"""
+    con = get_db()
+    cur = con.cursor()
+    cur.execute(f'''
+                select P2.Dex, P2.PokemonName from Pokemon P1, Pokemon P2
+                where P2.EvolvesFrom = P1.Dex and P1.PokemonName = "{pokemon_name}"
+                ''')
+
+    d = {}
+    for row in cur.fetchall():
+        for col in row.keys():
+            d[col] = row[col]
+
+    return json.dumps(d)
+
+
+@bp.route('/pokemon_with_move/<move_name>')
+def pokemon_with_move(move_name: str):
+    """Lists the evolutions of a pokemon"""
+    con = get_db()
+    cur = con.cursor()
+    cur.execute(f'''
+                select PokemonName, Learns.method from Pokemon natural join Learns natural join Move
+                where Move.MoveName = "{move_name}"
+                union
+                select PokemonName from Pokemon natural join LearnsByBreeding natural join Move
+                where Move.MoveName = "{move_name}"
+                ''')
+    # left and right side of unions do not have the same number of columns
+
+    d = {}
+    for row in cur.fetchall():
+        for col in row.keys():
+            d[col] = row[col]
 
     return json.dumps(d)
