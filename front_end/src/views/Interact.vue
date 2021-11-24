5676a74260a3ff5185e79874e68e9a140cdc4f61
jaredwebber <jaredwebberdev@gmail.com>
2021-11-24 11:29:38 -0600

    im gonna screm

--- front_end/src/views/Interact.vue
+++ front_end/src/views/Interact.vue
@@ -2,23 +2,34 @@
     <div class="INTERACThome">
         <h1>Interact with the Database!</h1>
 
-        <h3>Select which info you're looking for in the drop-down below</h3>
-    <v-container fluid>
+        <h3 id ="temp">Select which info you're looking for in the drop-down below</h3>
+      
+    <v-container fluid class="querySelect">
+      <v-row align="center">
+          <v-select 
+            v-model="selectQuery"
+            :items="queries"
+            item-text="query"
+            item-value="qid"
+            label="What do you want to know?"
+            persistent-hint
+            return-object
+            single-line
+          ></v-select>
+      </v-row>
+    </v-container>
+
+      <h3>BASED ON ABOVE QUERY, ADD MORE DROP-DOWNS TO SELECT PARAMETERS</h3>
+
+     <v-container fluid class="pokemonSelect">
       <v-row align="center">
         <v-col cols="6">
-          <v-subheader>
-            Custom items
-          </v-subheader>
-        </v-col>
-
-        <v-col cols="6">
           <v-select
-            v-model="select"
-            :hint="`${select.state}, ${select.abbr}`"
-            :items="items"
-            item-text="state"
-            item-value="abbr"
-            label="Select"
+            v-model="selectPokemon"
+            :items="pokemon"
+            item-text="pokemon"
+            item-value="pid"
+            label="Which pokemon are you looking for?"
             persistent-hint
             return-object
             single-line
@@ -27,35 +38,62 @@
       </v-row>
     </v-container>
 
-    <!-- this shit dont work https://vuetifyjs.com/en/components/selects/ -->
 
-            <br>
-            <h3>BASED ON ABOVE QUERY SELECTION, HAVE MORE DROP-DOWNS TO SELECT PARAMETERS</h3>
-            <h3>Probably use a different auto-complete type which shows all options easily</h3>
-
-            <!-- Maybe use combobox instead - only allow single selection at a time ?
-                  with 'change' event to trigger parameter popup options?-->
-
+    <!-- temp display of what is currently selected -->
+    <label id="query">
+      {{updateQuery}}
+    </label>
 
     </div>
 </template>
 
-<script>
-  export default {
-    data () {
+<script> 
+ export default {
+  data () {
       return {
-        select: { state: 'Florida', abbr: 'FL' },
-        items: [
-          { state: 'Florida', abbr: 'FL' },
-          { state: 'Georgia', abbr: 'GA' },
-          { state: 'Nebraska', abbr: 'NE' },
-          { state: 'California', abbr: 'CA' },
-          { state: 'New York', abbr: 'NY' },
+        selectQuery: { query: 'query', qid: '0' },
+        queries: [
+          { query: 'query1', qid: '1' },
+          { query: 'query2', qid: '2' },
+          { query: 'query3', qid: '3' },
+          { query: 'query4', qid: '4' },
+          { query: 'query5', qid: '5' },
+        ],
+
+        selectPokemon: { pokemon: 'pokemon', pid: '0' },
+        pokemon: [
+          { pokemon: 'pokemon1', pid: '1' },
+          { pokemon: 'pokemon2', pid: '2' },
+          { pokemon: 'pokemon3', pid: '3' },
+          { pokemon: 'pokemon4', pid: '4' },
+          { pokemon: 'pokemon5', pid: '5' },
         ],
       }
     },
+
+  computed: {
+    //make sure all appropriate values are selected for selected query
+    validateQuery(){
+      if(this.selectQuery.qid!=0 && this.selectPokemon.pid != 2){//make sure to change these tests
+        return true;
+      } 
+      return false;
+    },
+
+    //should be triggered by a 'query' button
+    updateQuery(){
+      if(this.validateQuery){ //literally why is this a value not a function with () !!!!!!!!!!!!!!!!
+        return "gucci gang";
+      }else{
+        return "missing params"
+      }
+    },
   }
+}
+
 </script>
+
+
 
 <style>
 
@@ -63,8 +101,8 @@
       padding:25px;
   }  
 
-  .querySelector{
-
-    }
+  .pokemonSelect{
+    visibility: visible;
+  }
 
 </style>
