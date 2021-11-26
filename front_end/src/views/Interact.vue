97896897ab4cb82b748e3d2257db8e617e0334f9
jaredwebber <jaredwebberdev@gmail.com>
2021-11-25 21:30:07 -0600

    added autocomplete

--- front_end/src/views/Interact.vue
+++ front_end/src/views/Interact.vue
@@ -9,8 +9,8 @@
           <v-select 
             v-model="selectQuery"
             :items="queries"
-            item-text="query"
-            item-value="qid"
+            item-text="value"
+            item-value="id"
             label="What do you want to know?"
             persistent-hint
             return-object
@@ -24,16 +24,17 @@
      <v-container fluid class="selectPokemon" v-if="pokemonVisible">
       <v-row align="center">
         <v-col cols="6">
-          <v-select
+          <v-autocomplete
             v-model="selectPokemon"
             :items="pokemon"
-            item-text="pokemon"
-            item-value="pid"
+            item-text="value"
+            item-value="id"
             label="Which pokemon are you looking for?"
             persistent-hint
             return-object
             single-line
-          ></v-select>
+            filled
+          ></v-autocomplete>
         </v-col>
       </v-row>
     </v-container>
@@ -41,16 +42,17 @@
     <v-container fluid class="selectLocation" v-if="locationVisible">
       <v-row align="center">
         <v-col cols="6">
-          <v-select
+          <v-autocomplete
             v-model="selectLocation"
             :items="locations"
-            item-text="location"
-            item-value="lid"
+            item-text="value"
+            item-value="id"
             label="Which location are you looking for?"
             persistent-hint
             return-object
             single-line
-          ></v-select>
+            filled
+          ></v-autocomplete>
         </v-col>
       </v-row>
     </v-container>
@@ -58,16 +60,17 @@
      <v-container fluid class="selectMove" v-if="moveVisible">
       <v-row align="center">
         <v-col cols="6">
-          <v-select
+          <v-autocomplete
             v-model="selectMove"
             :items="moves"
-            item-text="Which move are you looking for?"
-            item-value="mid"
+            item-text="value"
+            item-value="id"
             label="Which move are you looking for?"
             persistent-hint
             return-object
             single-line
-          ></v-select>
+            filled
+          ></v-autocomplete>
         </v-col>
       </v-row>
     </v-container>
@@ -75,16 +78,35 @@
      <v-container fluid class="selectStat" v-if="statVisible">
       <v-row align="center">
         <v-col cols="6">
-          <v-select
+          <v-autocomplete
             v-model="selectStat"
             :items="stats"
-            item-text="stat"
-            item-value="sid"
+            item-text="value"
+            item-value="id"
             label="Which stat are you looking for?"
             persistent-hint
             return-object
             single-line
-          ></v-select>
+            filled
+          ></v-autocomplete>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container fluid class="selectEvolution" v-if="evolutionVisible">
+      <v-row align="center">
+        <v-col cols="6">
+          <v-autocomplete
+            v-model="selectEvolution"
+            :items="evolutions"
+            item-text="value"
+            item-value="id"
+            label="Which evolution are you looking for?"
+            persistent-hint
+            return-object
+            single-line
+            filled
+          ></v-autocomplete>
         </v-col>
       </v-row>
     </v-container>
@@ -108,93 +130,77 @@
         locationVisible: false,
         moveVisible: false,
         statVisible: false,
+        evolutionVisible: false,
         //add all other params here
 
 
-        selectQuery: { query: 'query', qid: '0' },
+        selectQuery: { value: 'query', id: '0' },
         queries: [
-          { query: 'What do you want to know?', qid: '0' },
-          { query: 'All stats from a pokemon', qid: '1' },
-          { query: 'All evolutions of a pokemon', qid: '2' },
-          { query: 'All pokemon that learn a certain move', qid: '3' },
-          { query: 'query4', qid: '4' },
-          { query: 'query5', qid: '5' },
+          { value: 'What do you want to know?', id: '0' },
+          { value: 'All stats from a pokemon', id: '1' },
+          { value: 'All evolutions of a pokemon', id: '2' },
+          { value: 'All pokemon that learn a certain move', id: '3' },
+          { value: 'query4', id: '4' },
+          { value: 'query5', id: '5' },
         ],
 
-        selectPokemon: { pokemon: 'pokemon', pid: '0' },
+        selectPokemon: { value: 'pokemon', id: '0' },
         pokemon: [
-          { pokemon: 'pokemon1', pid: '1' },
-          { pokemon: 'pokemon2', pid: '2' },
-          { pokemon: 'pokemon3', pid: '3' },
-          { pokemon: 'pokemon4', pid: '4' },
-          { pokemon: 'pokemon5', pid: '5' },
+          { value: 'testPokemon', id: '1' },
         ],
 
-        selectLocation: { location: 'location', lid: '0' },
+        selectLocation: { value: 'location', id: '0' },
         locations: [
-          { location: 'location1', lid: '1' },
-          { location: 'location2', lid: '2' },
-          { location: 'location3', lid: '3' },
-          { location: 'location4', lid: '4' },
-          { location: 'location5', lid: '5' },
-          { location: 'location1', lid: '6' },
-          { location: 'location2', lid: '7' },
-          { location: 'location3', lid: '8' },
-          { location: 'location4', lid: '9' },
-          { location: 'location5', lid: '10' },
-          { location: 'location1', lid: '11' },
-          { location: 'location2', lid: '12' },
-          { location: 'location3', lid: '13' },
-          { location: 'location4', lid: '14' },
-          { location: 'location5', lid: '15' },
-           { location: 'location1', lid: '16' },
-          { location: 'location2', lid: '17' },
-          { location: 'location3', lid: '18' },
+          { value: 'testLocation', id: '1' },
         ],
 
-        selectMove: { move: 'move', mid: '0' },
+        selectMove: { value: 'move', id: '0' },
         moves: [
-          { move: 'move', mid: '1' },
-          { move: 'move', mid: '2' },
-          { move: 'move', mid: '3' },
-          { move: 'move', mid: '4' },
-          { move: 'move', mid: '5' },
+          { value: 'testMove', id: '1' },
         ],
 
-        selectStat: { stat: 'stat', sid: '0' },
+        selectStat: { value: 'stat', id: '0'},
         stats: [
-          { stat: 'stat', sid: '1' },
-          { stat: 'stat', sid: '2' },
-          { stat: 'stat', sid: '3' },
-          { stat: 'stat', sid: '4' },
-          { stat: 'stat', sid: '5' },
+          { value: 'testStat'},
         ],
+
+        selectEvolutions: { value: 'evolution', id: '0'},
+        evolutions: [
+          { value: 'testEvolution', id: '1' },
+        ],
+        
       }
     },
 
   methods:{
-    setVisibilty(qid){ //use this function to make required dropdowns visible for appropriate queries
-      if(qid == 0){
-        //set all to false
+    setVisibilty(query){ //use this function to make required dropdowns visible for appropriate queries
+      this.setAllHidden();
+      switch(query){
+        case 1: {
+          this.statVisible = true;
+          break;
+        }
+        case 2:{
+          this.evolutionVisible = true;
+          break;
+        }
+      }
+    },
+
+    setAllHidden(){
         this.locationVisible = false;
         this.pokemonVisible = false;
         this.moveVisible = false;
         this.statVisible = false;
-      }else{ //determine which dropdowns are needed for qid
-        this.locationVisible = true;
-        this.pokemonVisible = true;
-        this.moveVisible = true;
-        this.statVisible = true;
-      }
+        this.evolutionVisible = false;
     }
-
   },
 
   computed: {
     //make sure all appropriate values are selected for selected query
-    validateQuery(){
-      this.setVisibilty(this.selectQuery.qid);
-      if(this.selectQuery.qid!=0){//make sure to change these tests
+    vaidateQuery(){
+      this.setVisibilty(this.selectQuery.id);
+      if(this.selectQuery.id!=0){//make sure to change these tests
         return true;
       } 
       return false;
@@ -202,8 +208,12 @@
 
     //should be triggered by a 'query' button - currently placeholder doesnt do shit
     updateQuery(){
-      if(this.validateQuery){
-        return "use this to diplay query results prolly";
+      if(this.vaidateQuery){
+        return "use this to diplay query results prolly" + 
+        "\n pokemon selected: "+ this.selectPokemon.id +
+        "\n location selected: "+ this.selectLocation.id +
+        "\n move selected: "+ this.selectMove.id +
+        "\n stat selected: "+ this.selectStat.value ;
       }
       return "";//required for computed property apparently
     },
