24150b29c604638d67eb7a4c0ffcb3c1c1892e7d
jaredwebber <jaredwebberdev@gmail.com>
2021-11-30 23:47:58 -0600

    who even knows

--- front_end/src/views/Interact.vue
+++ front_end/src/views/Interact.vue
@@ -331,6 +331,8 @@
 </template>
 
 <script> 
+const axios = require('axios');
+
 export default {
      data () {
       return {
@@ -14405,103 +14407,33 @@ export default {
             this.setAllHidden();
             this.resultsVisible = true;
 
-            //this.results.value = axios(this.apiStr.url);
-            this.results.value = this.apiStr.url;
+            //this is just a label which displays the generated url
+            this.results.value = this.apiStr.url;//will be removed
+
+            //this is the same label - just displaying json return to see if working (its not)
+            this.results.value = axios(this.apiStr.url);
+            //^^ i also dont know if im using axios right ._.
+
             this.queryResults = { // PASS API JSON RETURN HERE
                 data: [
                     {
-                    name: 'Frozen Yogurt',
-                    calories: 159,
-                    fat: 6.0,
-                    carbs: 24,
-                    protein: 4.0,
-                    iron: '1%',
+                    name: 'Item',
+                    item1: 0,
+                    item2: 0,
+                    item3: 0,
                     },
-                    {
-                    name: 'Ice cream sandwich',
-                    calories: 237,
-                    fat: 9.0,
-                    carbs: 37,
-                    protein: 4.3,
-                    iron: '1%',
-                    },
-                    {
-                    name: 'Eclair',
-                    calories: 262,
-                    fat: 16.0,
-                    carbs: 23,
-                    protein: 6.0,
-                    iron: '7%',
-                    },
-                    {
-                    name: 'Cupcake',
-                    calories: 305,
-                    fat: 3.7,
-                    carbs: 67,
-                    protein: 4.3,
-                    iron: '8%',
-                    },
-                    {
-                    name: 'Gingerbread',
-                    calories: 356,
-                    fat: 16.0,
-                    carbs: 49,
-                    protein: 3.9,
-                    iron: '16%',
-                    },
-                    {
-                    name: 'Jelly bean',
-                    calories: 375,
-                    fat: 0.0,
-                    carbs: 94,
-                    protein: 0.0,
-                    iron: '0%',
-                    },
-                    {
-                    name: 'Lollipop',
-                    calories: 392,
-                    fat: 0.2,
-                    carbs: 98,
-                    protein: 0,
-                    iron: '2%',
-                    },
-                    {
-                    name: 'Honeycomb',
-                    calories: 408,
-                    fat: 3.2,
-                    carbs: 87,
-                    protein: 6.5,
-                    iron: '45%',
-                    },
-                    {
-                    name: 'Donut',
-                    calories: 452,
-                    fat: 25.0,
-                    carbs: 51,
-                    protein: 4.9,
-                    iron: '22%',
-                    },
-                    {
-                    name: 'KitKat',
-                    calories: 518,
-                    fat: 26.0,
-                    carbs: 65,
-                    protein: 7,
-                    iron: '6%',
-                    },
+                    
                 ],
                 headers: [
                     {
-                    text: 'Dessert (100g serving)',
+                    text: 'Item)',
                     align: 'start',
                     sortable: true,
                     value: 'name',
                     },
-                    { text: 'Calories', value: 'calories' },
-                    { text: 'Fat (g)', value: 'fat' },
-                    { text: 'Carbs (g)', value: 'carbs' },
-                    { text: 'Protein (g)', value: 'protein' },
-                    { text: 'Iron (%)', value: 'iron' },
+                    { text: 'Header1', value: 'item1' },
+                    { text: 'Header2', value: 'item2' },
+                    { text: 'Header3', value: 'item3' },
                 ],}
             //disable table loading
             this.doLoad = false;
