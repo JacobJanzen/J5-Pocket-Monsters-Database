1530bcc90e9f697a313f3d5bced27f72b65b636c
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2021-12-06 17:41:22 -0600

    one more try

--- run-unix.sh
+++ run-unix.sh
@@ -8,6 +8,7 @@ function ctrl_c()
     echo
     echo "**THIS SHOULD APPEAR**"
     fg
+    pkill flask
     exit
 }
 
@@ -18,7 +19,7 @@ python3 -m venv venv
 pip install -r requirements
 export FLASK_APP=flaskr
 export FLASK_ENV=development
-flask run &
+flask run & 
 
 # start front end
 cd ../front_end
